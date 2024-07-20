2db250cd53b25cbf069793669e1f5b19176a92e6
newtondotcom <asphalt9fr@gmail.com>
2024-07-20 16:55:45 +0200

    feat : image component with correct source

---
+++ src/lib/components/editor/Image.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import type { ImageData } from '@editorjs/simple-image';
+
+	export let data: ImageData;
+</script>
+
+<div class="flex h-full w-full flex-col items-center justify-center py-4">
+	<img
+		src={data.file.url}
+		alt={data.caption}
+		class={cn(data.withBorder ? 'px-4' : '', data.stretched ? 'w-full' : '')}
+	/>
+	<p class="text-neutral-700">{data.caption}</p>
+</div>
