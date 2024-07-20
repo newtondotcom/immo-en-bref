dd16d1f6b1c4bc186ecb881270591d01c390f91d
newtondotcom <asphalt9fr@gmail.com>
2024-07-20 16:55:10 +0200

    feat : quote component

---
+++ src/lib/components/editor/Quote.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { QuoteData } from '@editorjs/quote';
+	export let data: QuoteData;
+	const test = data.text;
+	const caption = data.caption;
+	const alignment = data.alignment;
+</script>
+
+<div class="flex flex-col items-center justify-center">
+	<blockquote class=" mx-4 w-full rounded-lg bg-neutral-200 px-2 py-1">{@html test}</blockquote>
+	<p class="text-md italic text-neutral-900">{@html caption}</p>
+</div>
