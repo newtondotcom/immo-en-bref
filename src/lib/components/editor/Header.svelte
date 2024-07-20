fab952a049e06804cbd7012aa41444039a87198b
newtondotcom <asphalt9fr@gmail.com>
2024-07-20 16:55:25 +0200

    feat : header component with level

--- src/lib/components/editor/Header.svelte
+++ src/lib/components/editor/Header.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
-	export let text = 'Heading';
+	import type { HeaderData } from '@editorjs/header';
+
+	export let data: HeaderData;
 	export let level = 2;
 </script>
 
-<h1>{@html text}</h1>
+{#if level === 1}
+	<h1 class="text-3xl font-bold">{@html data.text}</h1>
+{:else if level === 2}
+	<h2 class="text-2xl font-bold">{@html data.text}</h2>
+{:else if level === 3}
+	<h3 class="text-xl font-bold">{@html data.text}</h3>
+{:else if level === 4}
+	<h4 class="text-lg font-bold">{@html data.text}</h4>
+{:else if level === 5}
+	<h5 class="text-base font-bold">{@html data.text}</h5>
+{:else if level === 6}
+	<h6 class="text-sm font-bold">{@html data.text}</h6>
+{/if}
