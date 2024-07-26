6391d9ba96982f9b5a65ee1f33574bed48ef75ce
newtondotcom <asphalt9fr@gmail.com>
2024-07-26 16:09:06 +0200

    feat : fetch article dynamically from api

--- pages/articles/[name].vue
+++ pages/articles/[name].vue
@@ -2,62 +2,21 @@
 const route = useRoute();
 const name = route.params.name;
 
-const schema = ref({
-	time: 1721479935690,
-	blocks: [
-		{
-			id: '4_RflBDpCZ',
-			type: 'header',
-			data: {
-				text: 'Heading',
-				level: 2
-			}
-		},
-		{
-			id: '2NOe-b5Dy-',
-			type: 'paragraph',
-			data: {
-				text: 'ceci est un texte<br>'
-			}
-		},
-		{
-			id: 'Z5Md8vsAdI',
-			type: 'paragraph',
-			data: {
-				text: 'Ceci est encore un test<br> zdq<b>dzqd</b>'
-			}
-		},
-		{
-			id: 'anDCHE_MXS',
-			type: 'quote',
-			data: {
-				text: 'Test',
-				caption: 'test de quote<br>',
-				alignment: 'left'
-			}
-		},
-		{
-			id: 'hZAjSnqYMX',
-			type: 'image',
-			data: {
-				file: {
-					url: '/favicon.png'
-				},
-				withBorder: false,
-				withBackground: false,
-				stretched: false,
-				caption: 'CodeX Code Camp 2019'
-			}
-		}
-	],
-	version: '2.30.2'
+const schema = ref({});
+
+onMounted(async () => {
+	const data = await $fetch('/api/main', {
+		query: { name: name }
+	});
+	console.log(data);
+	schema.value = data;
 });
 </script>
 
 <template>
 	<div class="flex w-full flex-col px-10 py-4">
 		<div v-for="block in schema.blocks">
-			<EditorRendererHeader v-if="block.type === 'header'" :data="block.data" />
+			<EditorRendererHeader v-if="block.type === 'header'" :data="block.data" :time="schema.time" />
 			<EditorRendererParagraph v-if="block.type === 'paragraph'" :data="block.data" />
 			<EditorRendererQuote v-if="block.type === 'quote'" :data="block.data" />
 			<EditorRendererImage v-if="block.type === 'image'" :data="block.data" />
