8c2c6533f6cb5a5fb3b209d82d8bc3d7ae3f01c5
newtondotcom <asphalt9fr@gmail.com>
2024-07-26 16:08:43 +0200

    feat : editor with options to save and discard changes

--- pages/edit.vue
+++ pages/edit.vue
@@ -2,12 +2,17 @@
 	<div class="flex flex-row">
 		<EditorListArticles :articles />
 
-		<div id="editorjs" class="w-[40vw] border-2 bg-neutral-50"></div>
+		<div id="editorjs" class="w-[40vw] border-2 bg-neutral-50 px-1 py-1"></div>
+
+		<Separator orientation="vertical" />
 
 		<div class="w-[40vw] border-2 border-red-400">
-			<!-- <Separator orientation="vertical" /> -->
 			<div v-for="block in schema.blocks">
-				<EditorRendererHeader v-if="block.type === 'header'" :data="block.data" />
+				<EditorRendererHeader
+					v-if="block.type === 'header'"
+					:data="block.data"
+					:time="schema.time"
+				/>
 				<EditorRendererParagraph v-if="block.type === 'paragraph'" :data="block.data" />
 				<EditorRendererQuote v-if="block.type === 'quote'" :data="block.data" />
 				<EditorRendererImage v-if="block.type === 'image'" :data="block.data" />
@@ -18,8 +23,10 @@
 	<div
 		class="fixed bottom-0 left-0 right-0 z-10 flex h-[50px] items-center justify-center border border-orange-500"
 	>
-		<Button class="mx-1" @click="save" variant="secondary">Annuler les modifications</Button>
-		<Button class="mx-1" @click="save">Sauvegarder</Button>
+		<Button class="mx-1" :disabled="!editionNonSaved" @click="save" variant="secondary">
+			Annuler les modifications
+		</Button>
+		<Button class="mx-1" :disabled="!editionNonSaved" @click="save"> Sauvegarder </Button>
 	</div>
 </template>
 
@@ -28,6 +35,8 @@ import Header from '@editorjs/header';
 import EditorJS from '@editorjs/editorjs';
 
 const schema = ref({});
+
+const editionNonSaved = ref(false);
 
 var editor = new EditorJS({
 	tools: {
