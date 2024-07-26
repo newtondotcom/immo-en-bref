5b1605ea3eee83a43fdda1fa9457b1aa134e5384
newtondotcom <asphalt9fr@gmail.com>
2024-07-26 12:17:03 +0200

    page with editor

---
+++ pages/edit.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <div id="editorjs"></div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import Header from '@editorjs/header';
+import EditorJS from '@editorjs/editorjs'
+
+var editor = new EditorJS({
+  tools: {
+    header: Header,
+  }
+});
+</script>
