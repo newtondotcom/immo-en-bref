e562fa6cab43c588dabb4eb5e8eabb856760f16a
newtondotcom <asphalt9fr@gmail.com>
2024-07-26 14:42:45 +0200

    feat : set first letter in maj

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function capitalizeFirstLetter(word: string) {
+	return word.charAt(0).toUpperCase() + word.slice(1);
+}
