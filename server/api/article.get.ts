2e5aedc3ecf4ec49e8f455379537dc7cddab21ca
newtondotcom <asphalt9fr@gmail.com>
2024-09-18 13:22:39 +0200

    fix : try/catch for unknown article

--- server/api/article.get.ts
+++ server/api/article.get.ts
@@ -5,9 +5,13 @@ const articles_path = 'articles/';
 export default defineEventHandler(async (event) => {
 	const query = getQuery(event);
 	const name = query.name;
+	try {
+		const content = fs.readFileSync(articles_path + name + '.html', 'utf-8');
 
-	const content = fs.readFileSync(articles_path + name + '.html', 'utf-8');
-
-	setResponseStatus(event, 200);
-	return content;
+		setResponseStatus(event, 200);
+		return content;
+	} catch (error) {
+		setResponseStatus(event, 404);
+		return 'Not found';
+	}
 });
