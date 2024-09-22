1282128a4276fab18a2b2fe1faf517be485339ec
newtondotcom <asphalt9fr@gmail.com>
2024-09-22 18:14:19 +0200

    feat : working recent articles list

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col">
-        <div class="py-4">
+        <div class="py-4 text-primary">
             <h1
                 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
             >
@@ -11,5 +11,44 @@
             </p>
         </div>
         <Separator />
+        <div v-for="article in articles" :key="article.name">
+            <NuxtLink
+                :to="'/articles/' + urlizeName(article.name)"
+                class="flex flex-row py-4 items-center justify-between"
+            >
+                <div class="flex flex-col">
+                    <h3 class="text-md font-semibold leading-7 text-gray-500 dark:text-gray-100">
+                        {{ new Date(article.date).toLocaleDateString('fr-FR') }}
+                    </h3>
+                    <h2
+                        class="text-2xl truncate font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14"
+                    >
+                        {{ article.name }}
+                    </h2>
+                </div>
+                <CircleChevronRight class="w-8 h-8 text-primary" />
+            </NuxtLink>
+            <Separator />
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+    import { unurlizeName, urlizeName } from '~/lib/utils';
+    import { CircleChevronRight } from 'lucide-vue-next';
+
+    const articles = ref([]);
+    async function getArticles() {
+        const data = await $fetch('/api/list');
+        data.list.map((article) =>
+            articles.value.push({
+                name: unurlizeName(article.title),
+                content: article.content,
+                date: article.date,
+            }),
+        );
+        console.log(articles.value);
+    }
+
+    getArticles();
+</script>
