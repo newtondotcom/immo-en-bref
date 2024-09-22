dc78e7f1b197a7351287f2c1c9594150befa5876
newtondotcom <asphalt9fr@gmail.com>
2024-09-22 18:14:40 +0200

    style : rewrite article page layout

--- pages/articles/[name].vue
+++ pages/articles/[name].vue
@@ -28,13 +28,22 @@
 </script>
 
 <template>
-    <div class="flex w-full flex-col px-10 py-4">
+    <div class="flex w-full flex-col py-4">
+        <div class="py-4">
+            <h1
+                className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
+            >
+                {{ nameHuman }}
+            </h1>
+            <p className="text-lg leading-7 text-gray-500 dark:text-gray-400"></p>
+        </div>
+        <article class="prose lg:prose-xl py-8" v-html="content" />
         <div id="bandeau" class="flex w-full flex-row rounded-xl bg-secondary px-4 py-2">
             <img src="/G.jpeg" class="h-16 w-16 rounded-full" />
             <div class="flex flex-col justify-center px-4">
                 <div>
                     Article écrit par
-                    <a href="https://www.linkedin.com/in/jerome-augereau/" class="text-blue-500">
+                    <a href="https://www.linkedin.com/in/jerome-augereau/" class="text-primary">
                         Jérôme Augereau
                     </a>
                 </div>
@@ -44,6 +53,5 @@
                 </div>
             </div>
         </div>
-        <article class="prose lg:prose-xl py-8" v-html="content" />
     </div>
 </template>
