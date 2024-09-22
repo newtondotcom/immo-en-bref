3743ae97f90f4fcad26d86ee29a634b21d5fdd86
newtondotcom <asphalt9fr@gmail.com>
2024-09-22 18:13:50 +0200

    style : layout for mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
     <body class="bg-secondary">
-        <header class="w-[60%] mx-auto">
+        <header class="w-full px-4 md:px-0 md:w-[60%] mx-auto">
             <div class="flex flex-row justify-between items-center py-20 mx-auto">
                 <NuxtLink to="/" class="text-black dark:text-white text-3xl font-bold">
-                    Immo en bref
+                    Immo-en-bref
                 </NuxtLink>
                 <div
                     class="flex flex-row space-x-2 items-center font-semibold text-black dark:text-white text-lg"
@@ -13,12 +13,18 @@
                 </div>
             </div>
         </header>
-        <div class="min-h-screen w-[60%] mx-auto">
+        <div class="min-h-screen w-full px-4 md:px-0 md:w-[60%] mx-auto">
             <slot />
         </div>
-        <footer class="h-[100px] w-[60%] mx-auto">
+        <footer class="h-[100px] w-full px-4 md:px-0 md:w-[60%] mx-auto">
+            <Separator class="mb-2" />
             Site en open source disponible
-            <a href="https://github.com/newtondotcom/immo-en-bref">ici</a>
+            <a class="text-primary" href="https://github.com/newtondotcom/immo-en-bref">ici</a>
+            , créé et imaginé par
+            <a class="text-primary" href="https://www.linkedin.com/in/robin-augereau/">
+                Robin Augereau
+            </a>
+            .
         </footer>
     </body>
 </template>
