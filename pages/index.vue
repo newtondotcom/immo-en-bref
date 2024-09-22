bad179a3369a4a3aab6306769f4c83a87fecdafa
newtondotcom <asphalt9fr@gmail.com>
2024-09-22 17:49:01 +0200

    feat: landing page draft

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,15 @@
-<template></template>
+<template>
+    <div class="flex flex-col">
+        <div class="py-4">
+            <h1
+                className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
+            >
+                Articles récents
+            </h1>
+            <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+                Bienvenue sur Immo-en-bref, le blog d'un journaliste passionné d'immobillier.
+            </p>
+        </div>
+        <Separator />
+    </div>
+</template>
